eb8be283e5be93adfb8b3983dabea2b3ceef0637
Daniel Kehoe <daniel@danielkehoe.com>
2020-07-27 18:44:17 +0800

    add <yax-tutorial-hero> tag

---
+++ scripts/custom-tags/yax-tutorial-hero.js
@@ -0,0 +1,75 @@
+// /scripts/custom-tags/yax-tutorial-hero.js file
+
+import { LitElement, html } from 'https://jspm.dev/lit-element';
+import { until } from 'https://jspm.dev/lit-html/directives/until.js';
+
+export class YaxTutorialHero extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  render() {
+    return html`
+    ${until(
+      fetch('./manifest.json')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Could not find manifest file');
+          }
+          return response.json();
+        })
+        .then(tutorial => {
+          let level_color = 'is-success'
+          switch (tutorial.level) {
+            case 'advanced':
+              level_color = 'is-warning'
+          }
+          return html`
+          <section class="hero has-background-white-ter">
+            <div class="hero-body">
+              <div class="container has-text-centered">
+                <h1 class="title">${tutorial.title}</h1>
+              </div>
+              <div class="field is-grouped is-grouped-multiline is-grouped-centered mt-5">
+                <div class="control">
+                  <div class="tags has-addons">
+                    <span class="tag is-dark">audience</span>
+                    <span class="tag is-info">${tutorial.audience}</span>
+                  </div>
+                </div>
+
+                <div class="control">
+                  <div class="tags has-addons">
+                    <span class="tag is-dark">level</span>
+                    <span class="tag ${level_color}">${tutorial.level}</span>
+                  </div>
+                </div>
+
+                <div class="control">
+                  <div class="tags has-addons">
+                    <span class="tag is-dark">topic</span>
+                    <span class="tag is-primary">${tutorial.topic}</span>
+                  </div>
+                </div>
+
+                <div class="control">
+                  <div class="tags has-addons">
+                    <span class="tag is-dark">subtopic</span>
+                    <span class="tag is-primary">${tutorial.subtopic}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          `;
+        })
+        .catch(error => {
+          console.error('Fetch failure:', error);
+          return html`<h4>Error</h4>`;
+        }),
+      html`<h4>Loading</h4>`
+    )}
+  `;
+  }
+}
+
+customElements.define('yax-tutorial-hero', YaxTutorialHero);
--- scripts/yax-tags-library.js
+++ scripts/yax-tags-library.js
@@ -2,10 +2,12 @@
 import { YaxMarkdown } from './custom-tags/yax-markdown.js';
 import { YaxFooter } from './custom-tags/yax-footer.js';
 import { YaxNavbar } from './custom-tags/yax-navbar.js';
+import { YaxTutorialHero } from './custom-tags/yax-tutorial-hero.js';
 const YaxTags = {
   YaxMarkdown,
   YaxFooter,
-  YaxNavbar
+  YaxNavbar,
+  YaxTutorialHero
 }
 
 export { YaxTags }
