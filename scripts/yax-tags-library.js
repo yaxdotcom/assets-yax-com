ff4458d57004f712a3fa303e5c093e9d28a43a2d
Daniel Kehoe <daniel@danielkehoe.com>
2020-07-28 19:02:33 +0800

    add <yax-tutorial-paginate> tag

--- scripts/yax-tags-library.js
+++ scripts/yax-tags-library.js
@@ -5,13 +5,15 @@ import { YaxNavbar } from './custom-tags/yax-navbar.js';
 import { YaxTutorialHero } from './custom-tags/yax-tutorial-hero.js';
 import { YaxTutorialToc } from './custom-tags/yax-tutorial-toc.js';
 import { YaxTutorialCrumbs } from './custom-tags/yax-tutorial-crumbs.js';
+import { YaxTutorialPaginate } from './custom-tags/yax-tutorial-paginate.js';
 const YaxTags = {
   YaxMarkdown,
   YaxFooter,
   YaxNavbar,
   YaxTutorialHero,
   YaxTutorialToc,
-  YaxTutorialCrumbs
+  YaxTutorialCrumbs,
+  YaxTutorialPaginate
 }
 
 export { YaxTags }
