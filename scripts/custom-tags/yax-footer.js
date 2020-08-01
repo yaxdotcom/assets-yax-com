63e997eaf065bc7c7e8bf7f297da2afff6610c32
Daniel Kehoe <daniel@danielkehoe.com>
2020-08-01 14:10:48 +0800

    get images from an assets server

--- scripts/custom-tags/yax-footer.js
+++ scripts/custom-tags/yax-footer.js
@@ -18,17 +18,17 @@ export class YaxFooter extends LitElement {
             <div>
               <a target="blank" href="https://github.com/yaxdotcom">
               <span class="icon  has-background-black">
-              <img src="//assets-yax-com/images/icons/github.jpg" alt="">
+              <img src="https://assets.yax.com/assets/images/icons/github.jpg" alt="">
               </span>
               </a>
               <a target="blank" href="https://twitter.com/rails_apps">
               <span class="icon">
-              <img src="//assets-yax-com/images/icons/twitter.svg" alt="">
+              <img src="https://assets.yax.com/assets/images/icons/twitter.svg" alt="">
               </span>
               </a>
               <a target="blank" href="https://www.facebook.com/daniel.kehoe.sf">
               <span class="icon">
-              <img src="//assets-yax-com/images/icons/facebook-f.svg" alt="">
+              <img src="https://assets.yax.com/assets/images/icons/facebook-f.svg" alt="">
               </span>
               </a>
             </div>
