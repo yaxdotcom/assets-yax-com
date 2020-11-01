af86ea7da326b0471e7715f86b4e03ae544726c7
Daniel Kehoe <daniel@danielkehoe.com>
2020-11-01 15:45:50 +0800

    avoid CSS name collision with yax-author-kehoe tg

--- scripts/tags-library/yax-author-kehoe.js
+++ scripts/tags-library/yax-author-kehoe.js
@@ -7,52 +7,52 @@ export class YaxAuthorKehoe extends LitElement {
 	render() {
 		return html`
       <style>
-        .card {
+        .card-author {
           border-radius: 6px;
           overflow: hidden;
           max-width: 300px;
           max-height: 380px;
           margin: 0 auto;
         }
-        .card .header {
+        .card-author .header {
           height: 60px;
           background: #e3efee;
         }
-        .card .header .avatar {
+        .card-author .header .avatar {
           width: 80px;
           height: 100%;
           position: relative;
           margin: 0 auto;
         }
-        .card .header .avatar img {
+        .card-author .header .avatar img {
           display: block;
           border-radius: 50%;
           position: absolute;
           bottom: -42px;
           border: 4px solid white;
         }
-        .card .card-body {
+        .card-author .card-body {
           padding: 30px;
         }
-        .card .card-body .author-meta {
+        .card-author .card-body .author-meta {
           padding-top: 20px;
         }
-        .card .card-body .author-meta .author-name {
+        .card-author .card-body .author-meta .author-name {
           font-size: 18px;
           font-weight: 600;
         }
-        .card .card-body .author-meta .author-about {
+        .card-author .card-body .author-meta .author-about {
           font-size: 90%;
           color: #8fada9;
         }
-        .card .author-bio {
+        .card-author .author-bio {
           padding-top: 8px;
           font-size: 92%;
           color: #999;
         }
       </style>
       <aside>
-      	<div class="card">
+      	<div class="card card-author">
       		<div class="header">
       			<div class="avatar">
       				<a href="https://danielkehoe.com/">
