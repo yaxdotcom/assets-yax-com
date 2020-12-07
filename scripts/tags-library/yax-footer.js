314c9a8ed29c79fd3482219b91918305028bd6fc
Daniel Kehoe <daniel@danielkehoe.com>
2020-12-07 10:00:14 +0000

    remove Crisp chat link and add Zulip community discussion

--- scripts/tags-library/yax-footer.js
+++ scripts/tags-library/yax-footer.js
@@ -64,10 +64,10 @@ export class YaxFooter extends LitElement {
           <div class="column">
           <h6 class="title is-6 has-text-white">Resources</h6>
           <ul style="list-style-type: circle; list-style-position: inside;">
-            <li><a href="#" onclick="$crisp.push(['do','chat:open'])">Live Chat</a></li>
-            <li><a href="mailto:support@yax.com">support@yax.com</a></li>
+            <li><a href="https://yax.zulipchat.com/">Community discussion</a></li>
             <li><a href="https://blog.yax.com/">Blog</a></li>
             <li><a href="https://yax.com/pricing.html">Pricing &amp; Comparison</a></li>
+            <li><a href="mailto:support@yax.com">support@yax.com</a></li>
           </ul>
           </div>
         </div>
