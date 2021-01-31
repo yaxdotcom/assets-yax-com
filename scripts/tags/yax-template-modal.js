c07190b9241efee998f8b9f36ddc74ba0a33740a
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-31 09:36:50 +0000

    use Skypack instead of JSPM

--- scripts/tags/yax-template-modal.js
+++ scripts/tags/yax-template-modal.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'https://jspm.dev/lit-element@2';
+import { LitElement, html } from 'https://cdn.skypack.dev/lit-element@2.4.0';
 
 export class YaxTemplateModal extends LitElement {
 
