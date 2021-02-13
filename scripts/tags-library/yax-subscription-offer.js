b83b7d38b4c6eb0755cc850fd8b9253fae1c9cf0
Daniel Kehoe <daniel@danielkehoe.com>
2021-02-13 08:23:06 +0000

    add analytics tracking for 'View Subscr Offer'

--- scripts/tags-library/yax-subscription-offer.js
+++ scripts/tags-library/yax-subscription-offer.js
@@ -13,9 +13,9 @@ export class YaxSubscriptionOffer extends LitElement {
           <div class="card-content">
             <div class="title has-text-weight-bold has-background-white">Stackless.</div>
             <p class="is-size-7 m-2 has-text-weight-medium">
-              Don't miss out on a new thing. Go to <span class="has-text-weight-semibold"><a href="https://stackless.community/">stackless.community</a></span> and get the newsletter.
+              Don't miss out on a new thing. Go to <span class="has-text-weight-semibold"><a @click="analytics.track('View Subscr Offer')" href="https://stackless.community/">stackless.community</a></span> and get the newsletter.
             </p>
-            <a class="button is-rounded is-inverted has-text-weight-semibold mt-4" href="https://stackless.community/">Get the newsletter</a>
+            <a @click="analytics.track('View Subscr Offer')" class="button is-rounded is-inverted has-text-weight-semibold mt-4" href="https://stackless.community/">Get the newsletter</a>
           </div>
         </div>
       </div>
