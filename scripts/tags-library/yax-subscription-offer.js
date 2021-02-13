9113bcb2c1b3ba0054542e3102ceb333ff5039a4
Daniel Kehoe <daniel@danielkehoe.com>
2021-02-13 07:59:17 +0000

    improve the yax-subscription-offer ad

--- scripts/tags-library/yax-subscription-offer.js
+++ scripts/tags-library/yax-subscription-offer.js
@@ -7,23 +7,19 @@ export class YaxSubscriptionOffer extends LitElement {
 
 	render() {
 		return html`
-      <section id="ad">
-      <div class="container has-text-centered py-4">
-        <div class="columns is-centered">
-          <div class="column pricing-plan has-background-link-light" style="max-width: 270px;">
+      <aside class="mt-4 container has-text-centered" id="ad">
+      <div class="columns is-centered">
+        <div class="column card has-background-link-light" style="max-width: 270px;">
+          <div class="card-content">
             <div class="title has-text-weight-bold has-background-white">Stackless.</div>
-            <div class="has-background-link-light">
-              <p class="is-size-7 m-2 has-text-weight-medium">
+            <p class="is-size-7 m-2 has-text-weight-medium">
               Don't miss out on a new thing. Go to <span class="has-text-weight-semibold"><a href="https://stackless.community/">stackless.community</a></span> and get the newsletter.
-              </p>
-            </div>
-            <div class="">
-              <a class="button is-rounded is-inverted has-text-weight-semibold mt-4" href="https://stackless.community/">Get the newsletter</a>
-            </div>
+            </p>
+            <a class="button is-rounded is-inverted has-text-weight-semibold mt-4" href="https://stackless.community/">Get the newsletter</a>
           </div>
         </div>
       </div>
-      </section>
+      </aside>
     `;
 	}
 }
