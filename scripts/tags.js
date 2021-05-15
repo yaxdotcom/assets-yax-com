1347b06931bc7f9a2e9b6d80cb3a04110135a2e0
Daniel Kehoe <daniel@danielkehoe.com>
2021-05-15 15:53:20 +0800

    move custom tags to a flat file structure in /scripts folder

---
+++ scripts/tags.js
@@ -0,0 +1,36 @@
+import { YaxArticlesList } from './yax-articles-list.js';
+import { YaxAuthorKehoe } from './yax-author-kehoe.js';
+import { YaxFooter } from './yax-footer.js';
+import { YaxInfocardsBreadcrumbs } from './yax-infocards-breadcrumbs.js';
+import { YaxInfocardsHeading } from './yax-infocards-heading.js';
+import { YaxInfocards } from './yax-infocards.js';
+import { YaxMarkdown } from './yax-markdown.js';
+import { YaxNavbar } from './yax-navbar.js';
+import { YaxSubscriptionOffer } from './yax-subscription-offer.js';
+import { YaxTemplateCard } from './yax-template-card.js';
+import { YaxTemplateGallery } from './yax-template-gallery.js';
+import { YaxTemplateModal } from './yax-template-modal.js';
+import { YaxTutorialCrumbs } from './yax-tutorial-crumbs.js';
+import { YaxTutorialHero } from './yax-tutorial-hero.js';
+import { YaxTutorialPaginate } from './yax-tutorial-paginate.js';
+import { YaxTutorialToc } from './yax-tutorial-toc.js';
+const YaxTags = {
+	YaxArticlesList,
+	YaxAuthorKehoe,
+  YaxFooter,
+  YaxInfocardsBreadcrumbs,
+  YaxInfocardsHeading,
+  YaxInfocards,
+  YaxMarkdown,
+  YaxNavbar,
+  YaxSubscriptionOffer,
+  YaxTemplateCard,
+  YaxTemplateGallery,
+  YaxTemplateModal,
+  YaxTutorialCrumbs,
+  YaxTutorialHero,
+  YaxTutorialPaginate,
+  YaxTutorialToc
+};
+
+export { YaxTags };
